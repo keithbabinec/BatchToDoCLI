c03a8a97b2872deab3a18f0c52f301a43fee5274
Keith Babinec <keith.babinec@gmail.com>
2022-03-12 00:19:01 -0800

    Adding date evaluation: part 1

--- BatchToDoCLI/Definitions/Transformer.cs
+++ BatchToDoCLI/Definitions/Transformer.cs
@@ -1,15 +1,18 @@
-﻿using BatchToDoCLI.Models;
+﻿using BatchToDoCLI.Exceptions;
+using BatchToDoCLI.Models;
 using System.Text.RegularExpressions;
 
 namespace BatchToDoCLI.Definitions
 {
     public class Transformer
     {
-        private Regex VariableMatcher;
+        private Regex StrVariableMatcher;
+        private Regex DateVariableMatcher;
 
         public Transformer()
         {
-            VariableMatcher = new Regex("{{\\w+}}");
+            StrVariableMatcher = new Regex("{{\\w+}}");
+            DateVariableMatcher = new Regex("{{\\w+[+|-]\\d+[d|w|m|y]}}");
         }
 
         public TaskBatch TransformFromVariables(TaskBatch template, Variables userVars)
@@ -33,14 +36,15 @@ namespace BatchToDoCLI.Definitions
             
             newTask.Name = FillTextVariables(original.Name, vars);
             newTask.Description = FillTextVariables(original.Description, vars);
-            newTask.DueDate = FillDateExpression(original.DueDate, vars);
+            newTask.DueDate = original.DueDate;
+            newTask.Evaluated = FillDateExpression(original.DueDate, vars);
 
             return newTask;
         }
 
         private string FillTextVariables(string original, Variables vars)
         {
-            var matches = VariableMatcher.Matches(original);
+            var matches = StrVariableMatcher.Matches(original);
 
             if (matches.Count == 0)
             {
@@ -64,9 +68,69 @@ namespace BatchToDoCLI.Definitions
 
         private string FillDateExpression(string original, Variables vars)
         {
-            // todo: implement
+            var matches = DateVariableMatcher.Matches(original);
+            if (matches.Count == 0)
+            {
+                return original;
+            }
 
-            return original;
+            var filledString = original;
+
+            foreach (Match match in matches)
+            {
+                string varKey = match.Value.Substring(2, match.Value.Length - 4);
+                var varAndExp = varKey.Split('-', '+');
+                char splitChar = match.Value.Substring(varAndExp[0].Length + 2, 1).FirstOrDefault();
+
+                if (vars.ContainsKey(varAndExp[0]))
+                {
+                    try
+                    {
+                        filledString = EvaluateDateExpression(vars[varAndExp[0]], varAndExp[1], splitChar);
+                    }
+                    catch (Exception)
+                    {
+                        throw new InvalidDateExpressionException($"Couldn't parse the date expression: {match.Value}");
+                    }
+                }
+            }
+
+            return filledString;
+        }
+
+        private string EvaluateDateExpression(string date, string expression, char symbol)
+        {
+            DateTime parsedDate = DateTime.Parse(date);
+
+            var modifierType = expression.Last();
+            var modifierValue = expression.Take(expression.Length - 1).ToString();
+            var modifierAsInt = int.Parse(modifierValue);
+
+            switch (modifierType)
+            {
+                case 'd':
+                    {
+                        break;
+                    }
+                case 'w':
+                    {
+                        break;
+                    }
+                case 'm':
+                    {
+                        break;
+                    }
+                case 'y':
+                    {
+                        break;
+                    }
+                default:
+                    {
+                        throw new NotImplementedException($"Unexpected modifier type: {modifierType}");
+                    }
+            }
+
+            return "";
         }
     }
 }
---
+++ BatchToDoCLI/Exceptions/InvalidDateExpressionException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace BatchToDoCLI.Exceptions
+{
+    [Serializable]
+    internal class InvalidDateExpressionException : Exception
+    {
+        public InvalidDateExpressionException()
+        {
+        }
+
+        public InvalidDateExpressionException(string message) : base(message)
+        {
+        }
+
+        public InvalidDateExpressionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected InvalidDateExpressionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
